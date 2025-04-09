badbc98bd7b74db890e5b139ed757b2c54ef086f
karriekamau <karriekamau@gmail.com>
2025-04-09 23:14:13 +0300

    mongodbcredupdate

--- _config.js
+++ _config.js
@@ -2,8 +2,8 @@ var config = {}
 
 // Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://karriekamau:fWqB1wLEhz2QPOsf@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
-    development: 'mongodb+srv://karriekamau:fWqB1wLEhz2QPOsf@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-    test: 'mongodb+srv://karriekamau:fWqB1wLEhz2QPOsf@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
+    production: 'mongodb://karriekamau:fWqB1wLEhz2QPOsf@ac-dq4fdns-shard-00-00.hrahjml.mongodb.net:27017,ac-dq4fdns-shard-00-01.hrahjml.mongodb.net:27017,ac-dq4fdns-shard-00-02.hrahjml.mongodb.net:27017/?replicaSet=atlas-qsal1f-shard-0&ssl=true&authSource=admin&retryWrites=true&w=majority&appName=DevOPsCluster01',
+    development: 'mongodb://karriekamau:fWqB1wLEhz2QPOsf@ac-dq4fdns-shard-00-00.hrahjml.mongodb.net:27017,ac-dq4fdns-shard-00-01.hrahjml.mongodb.net:27017,ac-dq4fdns-shard-00-02.hrahjml.mongodb.net:27017/?replicaSet=atlas-qsal1f-shard-0&ssl=true&authSource=admin&retryWrites=true&w=majority&appName=DevOPsCluster01',
+    test: 'mongodb://karriekamau:fWqB1wLEhz2QPOsf@ac-dq4fdns-shard-00-00.hrahjml.mongodb.net:27017,ac-dq4fdns-shard-00-01.hrahjml.mongodb.net:27017,ac-dq4fdns-shard-00-02.hrahjml.mongodb.net:27017/?replicaSet=atlas-qsal1f-shard-0&ssl=true&authSource=admin&retryWrites=true&w=majority&appName=DevOPsCluster01'
 }
 module.exports = config;
